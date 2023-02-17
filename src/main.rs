f201f183ee83dbb37861465a2252af5e68c16afb
abouthugo <hugo@perdomo.dev>
2023-02-16 23:20:31 -0500

    phase 6: error handling

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,13 @@ fn main() {
         println!("Please input your guess.");
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess).expect("Faioled to read line");
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                continue;
+            }
+        };
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
